1f2a2db8b9df191eb679da6ed443c4a2e46bdbdf
yildiz <kadiryasir.ky@gmail.com>
2024-02-11 19:25:18 +0300

    düzeltme

--- libraries/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# CSV dosyasını oku
-df = pd.read_csv('data.csv')
-
-# 'district' sütununda "Sincan" ile başlayan satırları seç
-df_sincan = df[df['district'].str.startswith('Etimesgut')]
-
-print(df_sincan)
